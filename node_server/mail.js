633da62755d792c4e819fcf5115da1dfa59872ac
M <mail@mail.com>
2017-10-23 11:31:19 +0700

    fixed

--- node_server/mail.js
+++ node_server/mail.js
@@ -32,3 +32,37 @@ smtpTransport.sendMail(mail, function(error, response){
       console.log('send email success');
    }
 });
+
+
+console.log('222 sending....');
+var smtp = {
+  host: 'mail.charityth.com', //set to your host name or ip
+  port: 25, //25, 465, 587 depend on your 
+  secure: true, // use SSL
+  auth: {
+    user: 'admin@charityth.com', //user account
+    pass: 'qqww1q2w' //user password
+  }
+};
+var smtpTransport = mailer.createTransport(smtp);
+
+var mail = {
+   from: 'admin@charityth.com',
+   to: 'k.m.ittipol@gmail.com',
+   subject: 'Sending Email using Node.js',
+   text: 'That was easy!'
+}
+
+smtpTransport.sendMail(mail, function(error, response){
+
+  console.log(error);
+
+   smtpTransport.close();
+   if(error){
+      //error handler
+      console.log('send error...');
+   }else{
+      //success handler 
+      console.log('send email success');
+   }
+});
