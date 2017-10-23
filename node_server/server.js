7e61b835363cf6ae11ad311eaacc5150efa34e08
M <mail@mail.com>
2017-10-23 11:10:23 +0700

    fixed

--- node_server/server.js
+++ node_server/server.js
@@ -349,38 +349,38 @@ io.on('connection', function(socket){
 
 });
 
-console.log('sending....');
-var smtp = {
-  host: '103.13.228.35', //set to your host name or ip
-  port: 587, //25, 465, 587 depend on your 
-  secure: true, // use SSL
-  auth: {
-    user: 'admin@charityth.com', //user account
-    pass: 'qqww1q2w' //user password
-  }
-};
-var smtpTransport = mailer.createTransport(smtp);
+// console.log('sending....');
+// var smtp = {
+//   host: '103.13.228.35', //set to your host name or ip
+//   port: 587, //25, 465, 587 depend on your 
+//   secure: true, // use SSL
+//   auth: {
+//     user: 'admin@charityth.com', //user account
+//     pass: 'qqww1q2w' //user password
+//   }
+// };
+// var smtpTransport = mailer.createTransport(smtp);
 
-var mail = {
-   from: 'admin@charityth.com',
-   to: 'k.m.ittipol@gmail.com',
-   subject: 'Sending Email using Node.js',
-   text: 'That was easy!'
-}
+// var mail = {
+//    from: 'admin@charityth.com',
+//    to: 'k.m.ittipol@gmail.com',
+//    subject: 'Sending Email using Node.js',
+//    text: 'That was easy!'
+// }
 
-smtpTransport.sendMail(mail, function(error, response){
+// smtpTransport.sendMail(mail, function(error, response){
 
-  console.log(error);
+//   console.log(error);
 
-   smtpTransport.close();
-   if(error){
-      //error handler
-      console.log('send error...');
-   }else{
-      //success handler 
-      console.log('send email success');
-   }
-});
+//    smtpTransport.close();
+//    if(error){
+//       //error handler
+//       console.log('send error...');
+//    }else{
+//       //success handler 
+//       console.log('send email success');
+//    }
+// });
 
 
 server.listen(env.SOCKET_PORT, env.SOCKET_HOST, () => {
