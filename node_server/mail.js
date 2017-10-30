598ffc7bc8d59b3bcb15da247bf129be0ded646e
M <mail@mail.com>
2017-10-30 19:46:05 +0700

    fixed

--- node_server/mail.js
+++ node_server/mail.js
@@ -2,7 +2,7 @@ var mailer = require("nodemailer");
 
 console.log('hhhhhhhhhhhhhhhhhhhhhh....');
 var smtp = {
-  host: '103.13.228.35', //set to your host name or ip
+  host: '127.0.0.1', //set to your host name or ip
   port: 25, //25, 465, 587 depend on your 
   secure: false, // use SSL
   auth: {
@@ -19,47 +19,6 @@ var mail = {
    from: 'admin@charityth.com',
    to: 'k.m.ittipol@gmail.com',
    subject: 'Sending Email using Node.js',
-   text: 'That was easy!'
-}
-
-smtpTransport.sendMail(mail, function(error, response){
-
-  console.log('xxxxxxxxxxxxxxxxxxxxx....');
-  console.log(error);
-
-   // smtpTransport.close();
-   // if(error){
-   //    //error handler
-   //    console.log('send error...');
-   // }else{
-   //    //success handler 
-   //    console.log('send email success');
-   // }
-});
-
-
-
-var mailer = require("nodemailer");
-
-console.log('hhhhhhhhhhhhhhhhhhhhhh....');
-var smtp = {
-  host: '103.13.228.35', //set to your host name or ip
-  port: 25, //25, 465, 587 depend on your 
-  secure: true, // use SSL
-  auth: {
-    user: 'admin', //user account
-    pass: 'as2w3e4r' //user password
-  }
-};
-
-console.log(smtp);
-
-var smtpTransport = mailer.createTransport(smtp);
-
-var mail = {
-   from: 'admin@charityth.com',
-   to: 'k.m.ittipol@gmail.com',
-   subject: 'hello Node.js',
    text: 'That was easy!'
 }
 
