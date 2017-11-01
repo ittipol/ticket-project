e21251b516ccacd8f4bc36910a062bc0d83320ab
M <mail@mail.com>
2017-11-01 18:35:12 +0700

    tracking

--- resources/views/shared/main.blade.php
+++ resources/views/shared/main.blade.php
@@ -1,10 +1,20 @@
 <!doctype html>
 <html>
 <head>
+  @if(env('APP_ENV') == 'production')
+  <!-- Global site tag (gtag.js) - Google Analytics -->
+  <script async src="https://www.googletagmanager.com/gtag/js?id=UA-109089944-1"></script>
+  <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', 'UA-109089944-1');
+  </script>
+  @endif
   <!-- Meta data -->
   @include('script.meta') 
   <!-- CSS & JS -->
-  <!-- @include('script.analyticstracking') -->
   @include('script.script')
 </head>
 <body>
