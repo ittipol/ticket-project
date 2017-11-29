47711ab05e1aa76b312140dc6e1ddafae8fe5723
M <mail@mail.com>
2017-11-29 10:51:35 +0700

    rich card

--- resources/views/shared/main.blade.php
+++ resources/views/shared/main.blade.php
@@ -37,14 +37,28 @@
     "@context": "http://schema.org",
     "@type": "Organization",
     "url": "https://ticketeasys.com/",
-    "name": "TicketEasys",
     "description": "เว็บไซต์ที่ให้คุณซื้อและขายบัตรงานแสดงต่างๆ ได้ด้วยตัวคุณเอง โดยคุณเป็นผู้ตั้งราคา",
-    "logo": "https://ticketeasys.com/assets/images/logo/logo_frc.jpg",
+    "logo": "https://ticketeasys.com/assets/images/logo/logo_frc.jpg"
+  }
+  </script>
+  <script type="application/ld+json">
+  {
+    "@context": "http://schema.org",
+    "@type": "WebSite",
+    "url": "https://ticketeasys.com/",
     "potentialAction": {
       "@type": "SearchAction",
       "target": "https://ticketeasys.com/ticket?q={search_term_string}",
       "query-input": "required name=search_term_string"
-    },
+    }
+  }
+  </script>
+  <script type="application/ld+json">
+  {
+    "@context": "http://schema.org",
+    "@type": "Person",
+    "name": "TicketEasys",
+    "url": "https://ticketeasys.com/",
     "sameAs": [
       "https://www.facebook.com/Ticketeasys-919999211490459"
     ]
