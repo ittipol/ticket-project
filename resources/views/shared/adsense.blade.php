1b0334239fbfc0876ea5dad329f36b7923008c8d
M <mail@mail.com>
2018-01-09 09:53:36 +0700

    adsense

--- resources/views/shared/adsense.blade.php
+++ resources/views/shared/adsense.blade.php
@@ -1,10 +1,21 @@
+<!-- old -->
+<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+<!-- top ticket -->
+<ins class="adsbygoogle"
+     style="display:inline-block;width:728px;height:90px"
+     data-ad-client="ca-pub-3785637714027605"
+     data-ad-slot="9897074545"></ins>
+<script>
+(adsbygoogle = window.adsbygoogle || []).push({});
+</script>
+<!-- new -->
 <script type="text/javascript">
     google_ad_client = "ca-pub-3785637714027605";
     google_ad_slot = "9897074545";
     google_ad_width = 728;
     google_ad_height = 90;
 </script>
-<!-- top titket -->
+<!-- top ticket -->
 <script type="text/javascript"
 src="//pagead2.googlesyndication.com/pagead/show_ads.js">
 </script>
